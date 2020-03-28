2d801a933459403612ca46959a680e0c52ea6c5d
Filip Troníček <filip.tronicek@seznam.cz>
2020-03-28 11:40:15 +0100

    Update every second

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+function UpdateTime() {
 const startDate = '2020-03-16';
 
 const timeDiff  = (new Date(startDate)) - new Date();
@@ -6,3 +7,5 @@ const days      = timeDiff / (1000 * 60 * 60 * 24);
 document.getElementById("counter").innerText = Math.abs(days.toFixed(0))
 
 console.log(days);
+}
+setInterval(UpdateTime, 1000)
