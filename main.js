f221d8355cda695d0ace7b2f5da8bdc417106f7c
Filip Troníček <filip.tronicek@seznam.cz>
2020-03-29 12:31:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 function UpdateTime() {
-const startDate = '2020-03-16';
+  const startDate = "2020-03-16";
 
-const timeDiff  = (new Date(startDate)) - new Date();
-const days      = timeDiff / (1000 * 60 * 60 * 24);
+  const timeDiff = new Date(startDate) - new Date();
+  const days = timeDiff / (1000 * 60 * 60 * 24);
 
-document.getElementById("counter").innerText = Math.abs(days.toFixed(0))
+  document.getElementById("counter").innerText = Math.abs(days.toFixed(0));
 
-console.log(days);
+  console.log(days);
 }
-setInterval(UpdateTime, 1000)
+UpdateTime();
+setInterval(UpdateTime, 1000);
